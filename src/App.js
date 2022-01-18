df8a0ceb9f88c1797bd15eaa3d29579902b39644
Maverick0008 <93142563+Maverick0008@users.noreply.github.com>
2022-01-18 13:25:35 +0300

    add: detail component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-
+import Detail from './components/Detail';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path='/detail' element={ <Detail />} />
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
